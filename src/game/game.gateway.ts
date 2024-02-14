5de6b06c30237277dbc6ca70408038e6427250ca
o-ogie <wrld.worthy@gmail.com>
2024-02-14 14:27:08 +0000

    feat: timers

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -92,19 +92,20 @@ export class GameGateway {
       }
 
       const roomInfo: RoomInfo = this.rooms.get(roomIdParam);
-      roomInfo.players.set(nickname, client.id)
+      roomInfo.players.set(nickname, client.id);
       client.data = {
         nickname,
         roomId: roomIdParam,
       };
       client.join(`${roomIdParam}`);
       client.broadcast.to(`${roomIdParam}`).emit('userJoined', nickname);
-      client.emit('myName', nickname)
+      client.emit('myName', nickname);
       console.log(roomIdParam, '번방 유저 조인', roomInfo.players.size, '명 ', roomInfo.players);
       if (roomInfo.players.size == maxParam) {
-        console.log(roomIdParam, '번방 게임 시작!')
+        console.log(roomIdParam, '번방 게임 시작!');
         this.server.to(`${roomIdParam}`).emit('go', 'GO!');
-        this.roomTimers.set(roomIdParam, this.itemTimer(roomIdParam))
+        this.roomTimers.set(roomIdParam, this.itemTimer(roomIdParam));
+        this.gameTimer(roomIdParam);
         this.updateLastActiveTime(roomIdParam);
       }
     } catch (e) {
@@ -113,16 +114,44 @@ export class GameGateway {
     }
   }
 
+  gameTimer(roomId:number){
+    let maxTime = 240;
+    const intervalId = setInterval(() => {
+        const minutes = Math.floor(maxTime / 60);
+        const seconds = maxTime % 60;
+
+        // 남은 시간을 MM:SS 형식으로 출력
+        const time = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        console.log(time)
+        this.server.to(`${roomId}`).emit('timer', time);
+        maxTime -= 1;
+        if (maxTime < 0) {
+            clearInterval(intervalId);
+            const roomInfo = this.rooms.get(roomId);
+            const players = Array.from(roomInfo.players.keys());
+            players.forEach((player)=>{
+              roomInfo.gameOver.add(player);
+            })
+            this.gameEnd(roomId);
+            console.log("Game finished!");
+        }
+    }, 1000);
+  }
+
   itemTimer(roomId:number) {
+    let maxTime = 240;
     return setInterval(() => {
       const roomSockets = this.server.sockets.adapter.rooms.get(`${roomId}`);
-
+      maxTime -= 30;
       if (roomSockets) {
         roomSockets.forEach((socketId) => {
           const socket = this.server.sockets.sockets.get(socketId);
+          const { nickname } = socket.data;
+          const isOver = this.rooms.get(roomId).gameOver.has(nickname)
+
           const randomItem: Array<string> = this.randomItems(this.items);
-          if (socket) {
-            console.log(socket.data.nickname, '템 선택해보자~')
+          if (socket && !isOver && maxTime>30) {
+            console.log(socket.data.nickname, '템 선택해보자~', '남은 시간', maxTime)
             socket.emit('itemSelectTime', randomItem);
           }
         });
@@ -197,21 +226,28 @@ export class GameGateway {
   ) {
     const { roomId, nickname } = client.data
     const roomInfo: RoomInfo = this.rooms.get(roomId)
-    if (isOver) {
+
+    // const score = roomInfo.scores.get(nickname);
+    // const { data: result } = await axios.post('http://jeongminjo.shop:3000/user/score', {
+    //   nickname,
+    //   score
+    // });
+    // console.log(`점수 전송~ ${nickname}의 점수 ${score} 결과 : ${result}`)
+
+
+    this.gameEnd(roomId, nickname);
+  }
+
+  gameEnd(roomId:number, nickname?:string){
+    const roomInfo: RoomInfo = this.rooms.get(roomId)
+    if(nickname){
       roomInfo.gameOver.add(nickname)
       console.log(roomInfo.gameOver)
     }
 
-    const score = roomInfo.scores.get(nickname);
-    const { data: result } = await axios.post('http://localhost:3305/user/score', {
-      nickname,
-      score
-    });
-    console.log(`점수 전송~ ${nickname}의 점수 ${score} 결과 : ${result}`)
-
-
     if (roomInfo.gameOver.size === roomInfo.players.size) {
       clearInterval(this.roomTimers.get(roomId));
+      this.server.to(`${roomId}`).emit('gameEnd',true)
       console.log('퇴출 시작', roomInfo.gameOver)
       const users = roomInfo.players.values()
       Array.from(users).forEach((user) => {
@@ -254,6 +290,8 @@ export class GameGateway {
     client.broadcast.to(`${roomId}`).emit('selectedItem', item);
     console.log(nickname, '<< 아이템 사용', item, '<<<');
   }
+
+  
 }
 
 type world = {
