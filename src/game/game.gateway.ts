0a2d494f7e039a66d31e8bd0073a8db061f1fc5a
o-ogie <wrld.worthy@gmail.com>
2024-02-21 04:36:23 +0000

    fix: verify Token test

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -36,21 +36,22 @@ export class GameGateway {
     "BOMB", "FOG", "FLIP", "ROCK"
   ];
 
+  private userNumber:number = 1;
   constructor(private jwtService: JwtService) {}
 
   @WebSocketServer()
   server: Server;
   
 
-  @Cron(CronExpression.EVERY_10_SECONDS)
-  async findGhostRoom() {
-    const currentTime = Date.now();
-    this.rooms.forEach((room, roomId) => {
-      if (currentTime - room.lastActivate >= 30000) {
-        this.deleteRoom(roomId);
-      }
-    });
-  }
+  // @Cron(CronExpression.EVERY_10_SECONDS)
+  // async findGhostRoom() {
+  //   const currentTime = Date.now();
+  //   this.rooms.forEach((room, roomId) => {
+  //     if (currentTime - room.lastActivate >= 30000) {
+  //       this.deleteRoom(roomId);
+  //     }
+  //   });
+  // }
 
   private deleteRoom(roomId: number) {
     console.log('deleteRoom:',roomId,'번 방 삭제!!!!')
@@ -211,7 +212,8 @@ export class GameGateway {
 
   async handleConnection(client: Socket) {
     try {
-      const nickname: string = await this.verifyToken(client);
+      // const nickname: string = await this.verifyToken(client);
+      const nickname = this.userNumber++;
       const roomId: string | string[] = client.handshake.query.roomId;
       const max: string | string[] = client.handshake.query.max;
       const roomIdParam: number = this.parseQueryParam(roomId);
@@ -313,7 +315,7 @@ export class GameGateway {
     @ConnectedSocket() client: Socket,
     @MessageBody() score: number
   ) {
-    await this.verifyToken(client);
+    // await this.verifyToken(client);
     const { roomId, nickname } = client.data;
     const roomInfo = this.rooms.get(roomId);
     const prevScores = roomInfo.scores.get(nickname);
